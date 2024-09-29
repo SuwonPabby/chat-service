57be86003614950ffa0ae75c8266f459081f18bd
lee-euiji <leeuj9663@naver.com>
2024-09-29 13:59:51 +0900

    Initial Commit

---
+++ chat-service/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chat-service
+//
+//  Created by 이의진 on 9/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
